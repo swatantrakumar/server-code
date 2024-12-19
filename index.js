e52d5512c1653f08e3443d5166c5ef63610b9fef
Swatantra Kumar <rajswatantra9@gmail.com>
2024-12-19 17:41:29 +0530

    update code

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require("express");
 const mongoose = require("mongoose");
 const UserModule = require("./modules/user");
 const cors = require("cors");
+require('dotenv').config();
 
 
 const app = express();
